8108a5e56761ec4a76ead5bb131370653582c87d
maria-siddiqui <mariesid04@gmail.com>
2021-07-12 13:37:02 +0500

    Added person class

--- src/com/example/vaadin1/Person.java
+++ src/com/example/vaadin1/Person.java
@@ -1,5 +1,37 @@
 package com.example.vaadin1;
 
 public class Person {
+	
+	private long id;
+	private String name;
+	private int age;
+	private String status;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
 
 }
